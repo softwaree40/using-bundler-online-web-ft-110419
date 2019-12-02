d91db84c471f12ce4a014d403b1628fc4fc2659f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-02 18:25:14 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,18 @@
-Hashie
-Sinatra
-Octokit
-Pry
-AwesomePrint
+require_relative "../bin/run.rb"
+begin
+  Hashie
+  Sinatra
+  Octokit
+  Pry
+  AwesomePrint
+    raise HashieError
+  rescue => e
+    puts e.message
+end
+
+
+class HashieError < StandardError
+  def message
+    "Hashie error has occured"
+  end
+end
